0dcc8104ed5dff4ccfbb1c8da3872d29d68fffb9
Roman Simecek <roman.simecek@cyt.ch>
2011-08-09 10:02:00 +0200

    Recipe bug fixed.

--- lib/recipes/thinking_sphinx/thinking_sphinx.rb
+++ lib/recipes/thinking_sphinx/thinking_sphinx.rb
@@ -1,23 +1,25 @@
 # Thinking Sphinx for Capistrano
 require 'thinking_sphinx/deploy/capistrano'
 
-after "deploy:setup", "thinking_sphinx:setup"
+Capistrano::Configuration.instance.load do
+  after "deploy:setup", "thinking_sphinx:setup"
 
-after "deploy:migrate" do
-  thinking_sphinx.symlink
-  thinking_sphinx.rebuild
-end
-
-namespace :thinking_sphinx do
-  desc "Prepare for sphinx config"
-  task :setup, :roles => :app do
-    run "mkdir -p #{shared_path}/config/sphinx"
-    run "mkdir -p #{shared_path}/db/sphinx"
+  after "deploy:migrate" do
+    thinking_sphinx.symlink
+    thinking_sphinx.rebuild
   end
 
-  desc "Make symlink for sphinx configs and data"
-  task :symlink, :roles => :app do
-    run "ln -nfs #{shared_path}/config/sphinx #{release_path}/config/sphinx"
-    run "ln -nfs #{shared_path}/db/sphinx #{release_path}/db/sphinx"
+  namespace :thinking_sphinx do
+    desc "Prepare for sphinx config"
+    task :setup, :roles => :app do
+      run "mkdir -p #{shared_path}/config/sphinx"
+      run "mkdir -p #{shared_path}/db/sphinx"
+    end
+
+    desc "Make symlink for sphinx configs and data"
+    task :symlink, :roles => :app do
+      run "ln -nfs #{shared_path}/config/sphinx #{release_path}/config/sphinx"
+      run "ln -nfs #{shared_path}/db/sphinx #{release_path}/db/sphinx"
+    end
   end
 end
