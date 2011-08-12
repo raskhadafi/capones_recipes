873d78f4f85502e48706df201b22dbe06135474d
Roman Simecek <roman.simecek@cyt.ch>
2011-08-12 12:01:23 +0200

    Kuhsaft sync down realized.

--- lib/recipes/database/sync.rb
+++ lib/recipes/database/sync.rb
@@ -70,6 +70,37 @@ Capistrano::Configuration.instance.load do
       end
 
       desc <<-DESC
+        Syncs database from the selected mutli_stage environement to the local develoment environment.
+        The database credentials will be read from your local config/database.yml file and a copy of the
+        dump will be kept within the shared sync directory. The amount of backups that will be kept is
+        declared in the sync_backups variable and defaults to 5.
+      DESC
+      task :kuhsaft, :roles => :db, :only => { :primary => true } do
+        # Use production on non-multistage
+        set :stage, 'production' unless exists?(:stage)
+
+        filename = "database.kuhsaft.#{stage}.#{Time.now.strftime '%Y-%m-%d_%H:%M:%S'}.sql.bz2"
+        on_rollback { delete "#{shared_path}/sync/#{filename}" }
+
+        # Remote DB dump
+        username, password, database, host = remote_database_config(stage)
+        host_option = host ? "--host='#{host}'" : ""
+        run "mysqldump -u #{username} --password='#{password}' #{host_option} #{database} pages localized_pages contents assets tags taggings | bzip2 -9 > #{shared_path}/sync/#{filename}" do |channel, stream, data|
+          puts data
+        end
+        purge_old_backups "database"
+
+        # Download dump
+        download "#{shared_path}/sync/#{filename}", filename
+
+        # Local DB import
+        username, password, database, host = database_config('development')
+        system "bzip2 -d -c #{filename} | mysql -u #{username} --password='#{password}' #{database}; rm -f #{filename}"
+
+        logger.important "sync database from the stage '#{stage}' to local finished"
+      end
+
+      desc <<-DESC
         Sync declared directories from the selected multi_stage environment to the local development
         environment. The synced directories must be declared as an array of Strings with the sync_directories
         variable. The path is relative to the rails root.
