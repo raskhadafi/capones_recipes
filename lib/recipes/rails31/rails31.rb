909a40200a47c0b6ed70f6e8b2c7f25829b04754
Roman Simecek <roman.simecek@cyt.ch>
2011-08-12 09:09:48 +0200

    Compile assets now done via bundler.

--- lib/recipes/rails31/rails31.rb
+++ lib/recipes/rails31/rails31.rb
@@ -1,6 +1,6 @@
 Capistrano::Configuration.instance.load do
   # Compile the assets in Rails 3.1
   after 'deploy:migrate' do
-    run "cd #{release_path}; RAILS_ENV=production rake assets:precompile"
+    run "cd #{release_path}; RAILS_ENV=#{rails_env} bundle exec rake assets:precompile"
   end
 end
