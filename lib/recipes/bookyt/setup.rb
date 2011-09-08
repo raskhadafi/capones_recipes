bd3d9d12d2b26c52dba50fe81acbd349e7041a9d
Roman Simecek <roman.simecek@cyt.ch>
2011-09-08 15:54:17 +0200

    New recipe for bookyt added.

---
+++ lib/recipes/bookyt/setup.rb
@@ -0,0 +1,16 @@
+Capistrano::Configuration.instance.load do
+  after "deploy:setup", "bookyt:setup"
+  before "deploy:migrate", "bookyt:symlink"
+
+  namespace :bookyt do
+    desc "Asks which modules should be initialized and writes the config/initializer/bookyt.rb"
+    task :setup, :roles => :app do
+      run "mkdir -p #{shared_path}/initializer"
+    end
+    
+    desc "Make symlink for shared bookyt initializer"
+    task :symlink do
+      run "ln -nfs #{shared_path}/initializer/bookyt.rb #{release_path}/initializer/bookyt.rb"
+    end
+  end
+end
