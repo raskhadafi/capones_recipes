483deaaac0e47e1e96c84db665031d51db3f9b11
Roman Simecek <roman.simecek@cyt.ch>
2011-09-26 12:28:03 +0200

    Corrected the path.

--- lib/recipes/airbrake/setup.rb
+++ lib/recipes/airbrake/setup.rb
@@ -7,7 +7,7 @@ Capistrano::Configuration.instance(true).load do
       api_key = utilities.ask('Please insert the API key.', '')
       run "mkdir -p #{shared_path}/config/initializers"
       initializer_template = File.expand_path(File.dirname(__FILE__) + '/templates/airbrake.rb')
-      utilities.init_file(initializer_template, "<%%>", api_key, "#{shared_path}/initializer/bookyt.rb")
+      utilities.init_file(initializer_template, "<%%>", api_key, "#{shared_path}/config/initializers/airbrake.rb")
     end
   end
 end
