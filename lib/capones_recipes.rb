2d7863e4834a9b13ff0d3b3fef0f36d2aeb6a029
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-10-20 00:50:38 +0200

    Don't simply require all tasks and cookbooks.

--- lib/capones_recipes.rb
+++ lib/capones_recipes.rb
@@ -1,6 +1,3 @@
 require 'capistrano'
 require 'capistrano/cli'
 require 'capistrano_colors'
-
-Dir.glob(File.join(File.dirname(__FILE__), '/capones_recipes/tasks/*.rb')).sort.each { |lib| require lib }
-Dir.glob(File.join(File.dirname(__FILE__), '/capones_recipes/cookbook/*.rb')).sort.each { |lib| require lib }
