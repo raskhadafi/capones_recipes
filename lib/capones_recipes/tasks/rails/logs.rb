fe43b2b4c58ae6b33d97f871543305d7c625d5c0
Roman Simecek <roman.simecek@cyt.ch>
2011-10-31 07:48:59 +0100

    Capistrano recipe for watching the log files.

---
+++ lib/capones_recipes/tasks/rails/logs.rb
@@ -0,0 +1,14 @@
+Capistrano::Configuration.instance.load do
+  desc "Watch the log on the application server."
+  task :watch_logs, :role => [:app] do
+    log_file = "#{shared_path}/log/#{rails_env}.log"
+    
+    run "tail -f #{log_file}" do |channel, stream, data|
+      puts data if stream == :out 
+      if stream == :err 
+        puts "[Error: #{channel[:host]}->#{rails_env}] #{data}"
+        break
+      end     
+    end
+  end
+end
