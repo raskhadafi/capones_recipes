8a8a056f38e7814d27100c67e3160579f4984b06
Roman Simecek <roman.simecek@cyt.ch>
2011-11-04 15:04:54 +0100

    Removed the rails31 recipe cause the assets precompile is now in rails integrated.

--- lib/capones_recipes/tasks/rails31/rails31.rb
+++
@@ -1,6 +0,0 @@
-Capistrano::Configuration.instance.load do
-  # Compile the assets in Rails 3.1
-  after 'deploy:migrate' do
-    run "cd #{release_path}; RAILS_ENV=#{rails_env} bundle exec rake assets:precompile"
-  end
-end
