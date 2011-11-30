8b311077c5494022f447c5dcdd5a7f3a1fc117c6
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-11-30 10:11:37 +0100

    Drop application specific tasks for "katalog".

--- lib/capones_recipes/tasks/katalog/katalog.rb
+++
@@ -1,9 +0,0 @@
-Capistrano::Configuration.instance.load do
-  namespace :katalog do
-    desc "Create sphinx wordform and exception lists."
-    task :export_sphinx_lists, :roles => :app do
-      logger.info "Create sphinx wordform and exception lists."
-      run "cd #{deploy_to}/current && RAILS_ENV=#{rails_env} /usr/bin/env bundle exec rails runner 'SphinxAdminWordForm.send(:export_file); SphinxAdminException.send(:export_file)'"
-    end
-  end
-end
