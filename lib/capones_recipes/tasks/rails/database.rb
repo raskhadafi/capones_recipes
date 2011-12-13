13d3def9f1dda18872f7136d3b182fded55a4351
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-12-13 23:52:42 +0100

    New task db:configure for database.yml creation based on stage config.

--- lib/capones_recipes/tasks/rails/database.rb
+++ lib/capones_recipes/tasks/rails/database.rb
@@ -5,8 +5,52 @@ Capistrano::Configuration.instance.load do
   namespace :db do
     desc "Create database.yaml based on example"
     task :prepare_config do
+      run "mkdir -p #{shared_path}/db"
       run "mkdir -p #{shared_path}/config"
-      upload "config/database.yml.example", "#{shared_path}/config/database.yml", :via => :scp
+    end
+
+    # Author::      Simone Carletti <weppos@weppos.net>
+    # License::     MIT License
+    # Link::        http://www.simonecarletti.com/
+    # Source::      http://gist.github.com/2769
+    desc <<-DESC
+      Creates the database.yml configuration file in shared path.
+
+      By default, this task uses a template unless a template
+      called database.yml.erb is found either is :template_dir
+      or /config/deploy folders. The default template matches
+      the template for config/database.yml file shipped with Rails.
+
+      When this recipe is loaded, db:configure is automatically configured
+      to be invoked after deploy:setup. You can skip this task setting
+      the variable :skip_db_setup to true. This is especially useful
+      if you are using this recipe in combination with
+      capistrano-ext/multistaging to avoid multiple db:configure calls
+      when running deploy:configure for all stages one by one.
+    DESC
+    task :configure, :except => { :no_release => true } do
+
+      default_template = <<-EOF
+      base: &base
+        adapter: sqlite3
+        timeout: 5000
+      development:
+        database: #{shared_path}/db/development.sqlite3
+        <<: *base
+      test:
+        database: #{shared_path}/db/test.sqlite3
+        <<: *base
+      production:
+        database: #{shared_path}/db/production.sqlite3
+        <<: *base
+      EOF
+
+      location = fetch(:template_dir, "config/deploy") + '/database.yml.erb'
+      template = File.file?(location) ? File.read(location) : default_template
+
+      config = ERB.new(template)
+
+      put config.result(binding), "#{shared_path}/config/database.yml"
     end
 
     desc "Make symlink for shared database yaml"
