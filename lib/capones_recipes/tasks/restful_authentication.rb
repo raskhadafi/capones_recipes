a4e77d643a29e1a664d154c80a62914d82c50f21
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-12-14 11:12:20 +0100

    Add restful_authentication:configure task.

--- lib/capones_recipes/tasks/restful_authentication.rb
+++ lib/capones_recipes/tasks/restful_authentication.rb
@@ -1,10 +1,22 @@
 Capistrano::Configuration.instance.load do
+  before "deploy:setup", "restful_authentication:prepare_config"
   after "deploy:update_code", "restful_authentication:symlink"
+  before "restful_authentication:configure", "deploy:update_code"
 
   namespace :restful_authentication do
+    desc "Create shared directories"
+    task :prepare_config do
+      run "mkdir -p #{shared_path}/config/initializers"
+    end
+
     desc "Make symlink for site key"
     task :symlink do
       run "ln -nfs #{shared_path}/config/initializers/site_keys.rb #{latest_release}/config/initializers/site_keys.rb"
+    end
+
+    desc "Create new site_key"
+    task :configure, :roles => :app do
+      run("cd #{latest_release} && /usr/bin/env bundle exec rake auth:gen:site_key RAILS_ENV=#{rails_env}")
     end
   end
 
