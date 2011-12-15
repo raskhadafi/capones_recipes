7458da75acaca238bcd3b4d41f1245f0f76f3fc9
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-12-15 15:33:25 +0100

    New task email:configure.

--- lib/capones_recipes/tasks/email.rb
+++ lib/capones_recipes/tasks/email.rb
@@ -1,8 +1,28 @@
 # Email configuration
 Capistrano::Configuration.instance.load do
+  before "deploy:setup", "db:prepare_config"
   after "deploy:finalize_update", "email:symlink"
   
   namespace :email do
+    desc "Create shared directories"
+    task :prepare_config do
+      run "mkdir -p #{shared_path}/config/initializers"
+    end
+
+    desc "Configure email.yml"
+    task :configure, :except => {:no_release => true} do
+      default_template = <<-EOF
+      ActionMailer::Base.delivery_method = :sendmail
+      EOF
+
+      location = fetch(:template_dir, "config/deploy") + '/email.rb.erb'
+      template = File.file?(location) ? File.read(location) : default_template
+
+      config = ERB.new(template)
+
+      put config.result(binding), "#{shared_path}/config/initializers/email.rb"
+    end
+
     desc "Make symlink for email config"
     task :symlink, :roles => :app do
       run "ln -nfs #{shared_path}/config/initializers/email.rb #{latest_release}/config/initializers/email.rb"
