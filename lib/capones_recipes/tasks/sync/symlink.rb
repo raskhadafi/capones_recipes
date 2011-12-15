16968532107fb4d27881714affd167a389b6d2f3
Roman Simecek <roman.simecek@cyt.ch>
2011-12-15 09:03:37 +0100

    Set the symlinks for the sync_directories.

---
+++ lib/capones_recipes/tasks/sync/symlink.rb
@@ -0,0 +1,21 @@
+Capistrano::Configuration.instance.load do
+  #
+  # Capistrano task for syncing directories.
+  #
+  namespace :sync do
+    after "deploy:finalize_update", "sync:symlink"
+    after "sync:up:fs", "sync:symlink"
+    
+    desc "Sets the symlink to sync directories"
+    task :symlink do
+      Array(fetch(:sync_directories, [])).each do |syncdir|
+        unless File.directory? "#{syncdir}"
+          logger.info "Create '#{syncdir}' directory"
+          run "mkdir -p #{shared_path}/#{syncdir}"
+        end
+        
+        run "ln -nfs #{shared_path}/#{syncdir} #{latest_release}/#{syncdir}"
+      end
+    end
+  end
+end
