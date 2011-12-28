9d496b933ce8f7e81724f6326d8b41effebec10d
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-12-28 10:19:34 +0100

    Grant db access to servers with role :app.

--- lib/capones_recipes/tasks/database/setup.rb
+++ lib/capones_recipes/tasks/database/setup.rb
@@ -16,7 +16,7 @@ Capistrano::Configuration.instance.load do
     task :grant, :roles => :db_server do
       # TODO: support more than one server
       # TODO: lookup fqdn for web servers
-      web_server = find_servers(:roles => :web).first
+      web_server = find_servers(:roles => :app).first
       run "mysql -u root -p #{db_database} -e \"GRANT ALL PRIVILEGES ON #{db_database}.* TO '#{db_username}'@'#{web_server}' IDENTIFIED BY '#{db_password}'\"" do |channel, stream, data|
         if data =~ /^Enter password:/
           logger.info "#{channel[:host]} asked for password"
