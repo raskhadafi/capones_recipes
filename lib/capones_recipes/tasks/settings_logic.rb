7e9f2e9cf298296db705b030483874fe065f7f16
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2012-02-28 10:59:12 +0100

    Drop settings_logic.prepare_config; use deploy:finalize_update as trigger for settings_logic:symlink.

--- lib/capones_recipes/tasks/settings_logic.rb
+++ lib/capones_recipes/tasks/settings_logic.rb
@@ -1,13 +1,7 @@
 Capistrano::Configuration.instance.load do
-  before "db:symlink", "settings_logic:symlink"
+  before "deploy:finalize_update", "settings_logic:symlink"
 
   namespace :settings_logic do
-    desc "Create application yaml in capistrano shared path"
-    task :prepare_config do
-      run "mkdir -p #{shared_path}/config"
-      upload "config/application.yml.example", "#{shared_path}/config/application.yml", :via => :scp
-    end
-
     desc "Make symlink for shared application yaml"
     task :symlink do
       run "ln -nfs #{shared_path}/config/application.yml #{latest_release}/config/application.yml"
