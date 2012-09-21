cb9264217b085b2fc1846b20254c7e333230be16
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2012-09-21 15:30:42 +0200

    Use FQDN for web servers when granting DB permissions.

--- lib/capones_recipes/tasks/database/setup.rb
+++ lib/capones_recipes/tasks/database/setup.rb
@@ -15,9 +15,10 @@ Capistrano::Configuration.instance.load do
     desc "Grant access"
     task :grant, :roles => :db_server do
       # TODO: support more than one server
-      # TODO: lookup fqdn for web servers
       web_server = find_servers(:roles => :app).first
-      run "mysql -u root -p #{db_database} -e \"GRANT ALL PRIVILEGES ON #{db_database}.* TO '#{db_username}'@'#{web_server}' IDENTIFIED BY '#{db_password}'\"" do |channel, stream, data|
+      # lookup fqdn for web servers
+      fqdn_web_server = `host #{web_server} | cut -f 1 -d ' '`.delete("\n")
+      run "mysql -u root -p #{db_database} -e \"GRANT ALL PRIVILEGES ON #{db_database}.* TO '#{db_username}'@'#{fqdn_web_server}' IDENTIFIED BY '#{db_password}'\"" do |channel, stream, data|
         if data =~ /^Enter password:/
           logger.info "#{channel[:host]} asked for password"
           channel.send_data "\n"
